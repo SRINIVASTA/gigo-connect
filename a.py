0e23a51259f4ed4257fd2bdfc62d8b75cf16bf28
T A Srinivas <tasrinivass@gmail.com>
2026-08-23 11:43:23 +0530

    Fix SCOPES formatting in a.py

--- a.py
+++ a.py
@@ -33,7 +33,7 @@ AUTH_URL = "https://xero.com"
 TOKEN_URL = "https://xero.com"
 TENANT_API_URL = "https://xero.com"
 INVOICES_API_URL = "https://xero.com"
-SCOPES = "openid%20profile%20email%20app.connections%20accounting.contacts%20accounting.invoices%20offline_access"
+SCOPES = "openid profile email app.connections accounting.contacts accounting.invoices offline_access"
 
 # Helper Export Converters
 def convert_df_to_csv(df):
